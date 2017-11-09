7e34568decab2164e1a586a0618549ad9eb3f8ea
李嘉银 <lagagggggg@mackintoshbook.lan>
2017-11-09 17:26:34 +0800

    Initial Commit

---
+++ Tomorrow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tomorrow
+//
+//  Created by 李嘉银 on 2017/11/9.
+//  Copyright © 2017年 李嘉银. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tomorrow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tomorrow
+//
+//  Created by 李嘉银 on 2017/11/9.
+//  Copyright © 2017年 李嘉银. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
